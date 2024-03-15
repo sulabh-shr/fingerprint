d30253a2fcdc52346a6239017e497b22c9683217
Sulabh Shrestha <sulabh.shr@gmail.com>
2024-03-15 14:54:41 -0400

    Add timm model checks

--- scripts/checks/check_timm_model.py
+++ scripts/checks/check_timm_model.py
@@ -21,25 +21,25 @@ class Temp(torch.nn.Module):
 # model_name = "vit_large_patch16_384.augreg_in21k_ft_in1k"
 # model_name = "vit_huge_patch14_224.orig_in21k"
 # model_name = "swin_large_patch4_window12_384.ms_in22k"
-model_name = "swin_base_patch4_window12_384.ms_in22k_ft_in1k"
+# model_name = "swin_base_patch4_window12_384.ms_in22k_ft_in1k"
 # model_name = "efficientvit_m5.r224_in1k"
-use_img_size_config = True
+model_name = "mobilenetv3_large_100.ra_in1k"
+use_img_size_config = False
 
 device = 'cuda'
-img_size = 384
+img_size = 224
 if use_img_size_config:
     model = timm.create_model(model_name, pretrained=True, img_size=img_size, num_classes=256)
     model = model.to(device)
-    data_config = timm.data.resolve_model_data_config(model)
-    print(data_config)
 else:
     model = timm.create_model(model_name, pretrained=True, img_size=None, num_classes=256)
-
+    model.to(device)
+data_config = timm.data.resolve_model_data_config(model)
+print(data_config)
 x = torch.randn(2, 3, img_size, img_size).to(device)
 
 
 temp = Temp(model)
-print(temp)
 temp.train()
 print(f'Input shape: {x.shape}')
 # o = model(x)
--- scripts/checks/timm_model_list.py
+++ scripts/checks/timm_model_list.py
@@ -1,11 +1,12 @@
 import timm.models.vision_transformer
 import timm.models.swin_transformer
 
-# filter = '*efficient*'
+filter = '*mobile*'
 # filter = '*sam*'
 # filter = 'vit_huge*'
 # filter = 'vit_base*'
-filter = '*swin*'
+# filter = '*swin*'
+# filter = '*resnet50*'
 
 avail_pretrained_models = timm.list_models(filter, pretrained=True)
 for i in avail_pretrained_models:
