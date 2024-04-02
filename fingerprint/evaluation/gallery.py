87e6ec8072ef6c0aa6c23fc3a578bb4fa5fed3d3
Sulabh Shrestha <sulabh.shr@gmail.com>
2024-04-02 18:45:33 -0400

    Preprocess features before eval. Add cat fusion options

--- fingerprint/evaluation/gallery.py
+++ fingerprint/evaluation/gallery.py
@@ -1,9 +1,10 @@
 import torch
 import numpy as np
-from typing import List
+from typing import List, Union, Dict
 import torch.nn.functional as F
 import torch.distributed as dist
 from collections import defaultdict
+from sklearn.decomposition import PCA
 
 from .base import BaseEvaluator
 from .utils import scores_to_metrics
@@ -14,7 +15,7 @@ __all__ = ['FingerprintEvaluator', 'FingerprintEvaluatorWithLogits']
 class FingerprintEvaluator(BaseEvaluator):
     def __init__(self, dim=-1, fusion='score-avg', gallery_id='data1', probe_id='data2', device='cpu', verbose=True):
         super().__init__()
-        self.FUSIONS = ('score-avg', 'feat-avg')
+        self.FUSIONS = ('score-avg', 'feat-avg', 'cat-replicate', 'cat-pca')
         self.dim = dim
         assert fusion in self.FUSIONS, f'Invalid fusion: {fusion}. Must be one of {self.FUSIONS}'
         self.fusion = fusion
@@ -24,13 +25,15 @@ class FingerprintEvaluator(BaseEvaluator):
         self.device = device
         self.verbose = verbose
         self.similarity_fn = torch.nn.CosineSimilarity(dim=dim)
-        self.gallery = defaultdict(list)
-        self.probe = defaultdict(list)
+        self.gallery: Dict[str, Union[torch.Tensor, List[torch.Tensor]]] = defaultdict(list)
+        self.probe: Dict[str, Union[torch.Tensor, List[torch.Tensor]]] = defaultdict(list)
+        self.pca = None
         self.reset()
 
     def reset(self):
         self.gallery = defaultdict(list)
         self.probe = defaultdict(list)
+        self.pca = None
 
     def process(self, features: List[torch.Tensor], classes: List[str], locations: List[str]):
         for idx in range(len(features)):
@@ -50,8 +53,11 @@ class FingerprintEvaluator(BaseEvaluator):
         self.process(x2, key, ['data2'] * len(x1))
 
     def evaluate(self):
+        self._preprocess()
+
         y_true = []
         y_score = []
+
         for k2, probe_features in self.probe.items():
             k1_list = []
             k1_scores = []
@@ -79,7 +85,10 @@ class FingerprintEvaluator(BaseEvaluator):
 
         return result, fig
 
-    def score_gallery_probe(self, gallery: List[torch.Tensor], probe: List[torch.Tensor]):
+    def score_gallery_probe(self,
+                            gallery: Union[List[torch.Tensor], torch.Tensor],
+                            probe: Union[List[torch.Tensor], torch.Tensor]
+                            ) -> float:
         fusion = self.fusion
         if fusion == 'score-avg':
             scores = []
@@ -89,12 +98,45 @@ class FingerprintEvaluator(BaseEvaluator):
                     scores.append(score)
             final_score = np.mean(scores)
         elif fusion == 'feat-avg':
-            g = torch.stack(gallery).mean(dim=0)
-            p = torch.stack(probe).mean(dim=0)
-            final_score = self.score(g, p)
+            final_score = self.score(gallery, probe)
+        elif fusion in ('cat-pca', 'cat-replicate'):
+            n_g = len(gallery)
+            n_p = len(probe)
+            num_tiles = n_g // n_p
+            probe = probe.tile(num_tiles)
+            if n_g % n_p != 0:
+                probe = torch.cat([probe, probe[:n_g - len(probe)]])
+            final_score = self.score(gallery, probe)
         else:
             raise ValueError(f'Invalid fusion method: {fusion}')
         return final_score
+
+    def _preprocess(self):
+        if self.fusion == 'cat-pca':
+            print(f'Calculating PCA')
+            all_gallery_features = []
+            for k1, gallery_features in self.gallery.items():
+                for f in gallery_features:
+                    all_gallery_features.append(f.cpu().numpy())
+            all_gallery_features = np.array(all_gallery_features)
+            print(f'All Gallery Features: {all_gallery_features.shape}')
+            pca = PCA(n_components=32)
+            pca.fit(all_gallery_features)
+            self.pca = pca
+            for k1, gallery_features in self.gallery.items():
+                self.gallery[k1] = torch.from_numpy(self.pca.transform(np.array(gallery_features))).reshape(-1)
+            for k2, probe_features in self.probe.items():
+                self.probe[k2] = torch.from_numpy(self.pca.transform(np.array(probe_features))).reshape(-1)
+        elif self.fusion == 'cat-replicate':
+            for k1, gallery_features in self.gallery.items():
+                self.gallery[k1] = torch.cat(gallery_features)
+            for k2, probe_features in self.probe.items():
+                self.probe[k2] = torch.cat(probe_features)
+        elif self.fusion == 'feat-avg':
+            for k1, gallery_features in self.gallery.items():
+                self.gallery[k1] = torch.stack(gallery_features).mean(dim=0)
+            for k2, probe_features in self.probe.items():
+                self.probe[k2] = torch.stack(probe_features).mean(dim=0)
 
     def score(self, x1: torch.Tensor, x2: torch.Tensor):
         with torch.no_grad():
