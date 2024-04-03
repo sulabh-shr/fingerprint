dba336008e439d2ecb54a3e35ce9d337cbc5b617
Sulabh Shrestha <sulabh.shr@gmail.com>
2024-04-02 23:39:45 -0400

    Add command line option to preprocess

--- scripts/preprocess/preprocess_images.py
+++ scripts/preprocess/preprocess_images.py
@@ -1,34 +1,45 @@
 import os
 import cv2
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    from fingerprint.utils import crop_fingerprint, equalize_clahe
-    from fingerprint.utils import iterate_mmfv_files
 
-    from fingerprint_enhancer import FingerprintImageEnhancer
+def get_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--root', type=str, help='mmfv root path', required=True)
+    parser.add_argument('--out', type=str, help='output root path', required=True)
+    parser.add_argument('--crop', action='store_true', help='crop around finger tip', default=False)
+    parser.add_argument('--hist', action='store_true', help='equalize histogram', default=False)
+    parser.add_argument('--binarize', action='store_true', help='photo to fingerprint', default=False)
+    parser.add_argument('--ext', type=str, help='output extension', default='.jpg')
+    parser.add_argument('--overwrite', action='store_true', help='overwrite existing out files', default=False)
+    args = parser.parse_args()
+    return args
 
-    crop = True
-    hist = True
-    enhance = False
-    out_ext = '.jpg'
 
-    overwrite = False
-
-    root = r"D:\workspace\datasets\MMFV-10th-start_0"
-    # out_root = r"D:\workspace\datasets\MMFV-10th-start_0_processed"
-    out_root = r"D:\workspace\datasets\MMFV-10th-start_0_cropped"
+def main(args):
+    root = args.root
+    out_root = args.out
+    crop = args.crop
+    hist = args.hist
+    binarize = args.binarize
+    out_ext = args.ext
+    overwrite = args.overwrite
 
     for d in iterate_mmfv_files(root, out_root=out_root):
         in_path = d['img_path']
         out_dir = d['out_dir']
         frame, _ = os.path.splitext(d['frame'])
+
+        os.makedirs(out_dir, exist_ok=True)
+
         out_path = os.path.join(out_dir, frame + out_ext)
 
         if not overwrite and os.path.isfile(out_path):
             print(f'File exists: {out_path}')
             continue
+
         print(out_path)
         img = cv2.imread(in_path)
 
@@ -37,7 +48,7 @@ if __name__ == '__main__':
             try:
                 cropped_img = crop_fingerprint(img, segment=True, channels='BGR', viz=False)
             except Exception as e:
-                print(f'Error: {in_path}')
+                print(f'Error for file: {in_path}')
                 print(str(e))
                 continue
 
@@ -46,7 +57,7 @@ if __name__ == '__main__':
             enhanced_img = equalize_clahe(cropped_img, channels='BGR')
 
         img_out_uint8 = enhanced_img
-        if enhance:
+        if binarize:
             img_gray = cv2.cvtColor(enhanced_img, cv2.COLOR_BGR2GRAY)
 
             # Contrast stretching:
@@ -74,3 +85,13 @@ if __name__ == '__main__':
 
         if img_out_uint8.shape[0] > 0 and img_out_uint8.shape[1] > 0:
             cv2.imwrite(out_path, img_out_uint8)
+
+
+if __name__ == '__main__':
+    from fingerprint.utils import crop_fingerprint, equalize_clahe
+    from fingerprint.utils import iterate_mmfv_files
+
+    from fingerprint_enhancer import FingerprintImageEnhancer
+
+    _args = get_args()
+    main(_args)
