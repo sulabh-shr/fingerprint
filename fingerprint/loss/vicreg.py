2d1037064e0830acb3bcc0a54096cde532ce6ec1
Sulabh Shrestha <sulabh.shr@gmail.com>
2024-07-05 13:40:44 -0400

    Add label option to VICReg loss

--- fingerprint/loss/vicreg.py
+++ fingerprint/loss/vicreg.py
@@ -1,9 +1,28 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.distributed as dist
 
 __all__ = ['VICRegLoss']
+
+
+def sample_unique_labels(labels: torch.Tensor):
+    sorted_inds = torch.argsort(labels)
+    unique_labels, counts = torch.unique(labels, return_counts=True)
+    sorted_inds = sorted_inds.cpu().tolist()
+    higher_counts = counts > 1
+    # Sample 1 embedding per label
+    if torch.any(higher_counts):
+        unique_indices = []
+        current_idx = 0
+        for c in counts:
+            random_index = np.random.choice(sorted_inds[current_idx:current_idx + c])
+            unique_indices.append(random_index)
+            current_idx = current_idx + c
+    else:
+        unique_indices = list(range(len(labels)))
+    return unique_indices
 
 
 class VICRegLoss(nn.Module):
@@ -15,9 +34,19 @@ class VICRegLoss(nn.Module):
         self.std_coeff = std_coeff
         self.cov_coeff = cov_coeff
 
-    def forward(self, x, y, **kwargs):
-        repr_loss = F.mse_loss(x, y)
+    def forward(self, x, y, label=None, eps=0.0001, **kwargs):
+        """
 
+        Args:
+            x: tensor of shape (B, D)
+            y: tensor of shape (B, D)
+            label: label for x and y that denotes class/identity
+            eps: small number added to variance
+            **kwargs:
+
+        Returns:
+
+        """
         if dist.is_initialized():
             x = torch.cat(FullGatherLayer.apply(x), dim=0)
             y = torch.cat(FullGatherLayer.apply(y), dim=0)
@@ -25,19 +54,33 @@ class VICRegLoss(nn.Module):
         batch_size = x.shape[0]
         num_features = x.shape[-1]
 
+        # Invariance Loss
+        repr_loss = F.mse_loss(x, y)
+
+        # Variance Loss
+        unique_x = x
+        unique_y = y
+        if label is not None:
+            unique_indices = sample_unique_labels(label)
+            unique_x = x[unique_indices]
+            unique_y = y[unique_indices]
+        unique_x = unique_x - unique_x.mean(dim=0)
+        unique_y = unique_y - unique_y.mean(dim=0)
+        unique_std_x = torch.sqrt(unique_x.var(dim=0) + eps)
+        unique_std_y = torch.sqrt(unique_y.var(dim=0) + eps)
+        std_loss = torch.mean(F.relu(1 - unique_std_x)) / 2 + torch.mean(F.relu(1 - unique_std_y)) / 2
+
+        # Covariance Loss
         x = x - x.mean(dim=0)
         y = y - y.mean(dim=0)
-
-        std_x = torch.sqrt(x.var(dim=0) + 0.0001)
-        std_y = torch.sqrt(y.var(dim=0) + 0.0001)
-        std_loss = torch.mean(F.relu(1 - std_x)) / 2 + torch.mean(F.relu(1 - std_y)) / 2
-
         cov_x = (x.T @ x) / (batch_size - 1)
         cov_y = (y.T @ y) / (batch_size - 1)
-        cov_loss = off_diagonal(cov_x).pow_(2).sum().div(
-            num_features
-        ) + off_diagonal(cov_y).pow_(2).sum().div(num_features)
+        cov_loss = (
+                off_diagonal(cov_x).pow_(2).sum().div(num_features)
+                + off_diagonal(cov_y).pow_(2).sum().div(num_features)
+        )
 
+        # Total Loss
         loss = (
                 self.sim_coeff * repr_loss
                 + self.std_coeff * std_loss
